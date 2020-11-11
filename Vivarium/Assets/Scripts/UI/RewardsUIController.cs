8a8718f9c2f680775089d21d78a58423fe89df88
saba-kal <38592380+saba-kal@users.noreply.github.com>
2020-11-11 13:57:39 -0500

    Updated rewards controller to give the reward to a random character.

--- Vivarium/Assets/Scripts/UI/RewardsUIController.cs
+++ Vivarium/Assets/Scripts/UI/RewardsUIController.cs
@@ -17,23 +17,25 @@ namespace Assets.Scripts.UI
         public Image Option1Icon;
         public Image Option2Icon;
         public Image Option3Icon;
-        private int selectedReward = 0;
+
+        private int _selectedReward = 0;
 
         private void Start()
         {
             Option1.onClick.AddListener(() =>
             {
-                selectedReward = 0;
+                _selectedReward = 0;
             });
             Option2.onClick.AddListener(() =>
             {
-                selectedReward = 1;
+                _selectedReward = 1;
             });
             Option3.onClick.AddListener(() =>
             {
-                selectedReward = 2;
+                _selectedReward = 2;
             });
         }
+
         public void ShowRewardsScreen(System.Action callback, List<Item> possibleRewards)
         {
             RewardScreen.SetActive(true);
@@ -48,12 +50,66 @@ namespace Assets.Scripts.UI
 
                 NextLevel.onClick.AddListener(() =>
                 {
-                    InventoryManager.PlacePlayerItem(possibleRewards[selectedReward]);
+                    PlaceSelectedReward(rewards.ToList());
                     LogPlayerInventory();
                     callback();
                     RewardScreen.SetActive(false);
                 });
             }
+        }
+
+        private void PlaceSelectedReward(List<Item> possibleRewards)
+        {
+            Item reward = GetSelectedReward(possibleRewards);
+            if (reward == null)
+            {
+                return;
+            }
+
+            var randomCharacterId = GetRandomCharacterIdWithEmptyInventory();
+            if (string.IsNullOrEmpty(randomCharacterId))
+            {
+                return;
+            }
+
+            InventoryManager.PlaceCharacterItem(randomCharacterId, reward);
+        }
+
+        public Item GetSelectedReward(List<Item> possibleRewards)
+        {
+            if (_selectedReward < possibleRewards.Count)
+            {
+                return possibleRewards[_selectedReward];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public string GetRandomCharacterIdWithEmptyInventory()
+        {
+            var playerCharacters = TurnSystemManager.Instance?.PlayerController?.PlayerCharacters;
+            if (playerCharacters == null)
+            {
+                return null;
+            }
+
+            var characterIdsWithEmptyInventory = new List<string>();
+            foreach (var characterController in playerCharacters)
+            {
+                if (InventoryManager.GetCharacterItemCount(characterController.Id) < Constants.MAX_CHARACTER_ITEMS)
+                {
+                    characterIdsWithEmptyInventory.Add(characterController.Id);
+                }
+            }
+
+            if (characterIdsWithEmptyInventory.Count == 0)
+            {
+                return null;
+            }
+
+            return characterIdsWithEmptyInventory[Random.Range(0, characterIdsWithEmptyInventory.Count)];
         }
 
         private void LogPlayerInventory()
