1ee671ebfdaee0dfa097f375beb5dffad9449ffa
saba-kal <38592380+saba-kal@users.noreply.github.com>
2021-01-09 14:36:41 -0500

    Implemented a bunch of polish changes.

---
+++ Vivarium/Assets/Scripts/Common/ActivateOnKeyPress.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateOnKeyPress : MonoBehaviour
+{
+    public GameObject ObjectToActivate;
+    public KeyCode KeyPressed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyPressed))
+        {
+            ObjectToActivate.SetActive(!ObjectToActivate.activeSelf);
+        }
+    }
+}
--- Vivarium/Assets/Scripts/UI/HealthBar.cs
+++ Vivarium/Assets/Scripts/UI/HealthBar.cs
@@ -1,6 +1,9 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Collections;
+using System.Collections.Generic;
+using System;
 
 [RequireComponent(typeof(Slider))]
 public class HealthBar : MonoBehaviour
@@ -8,9 +11,41 @@ public class HealthBar : MonoBehaviour
     public Slider HealthBarSlider;
     public TextMeshProUGUI HealthBarText;
     public bool isShieldBar;
+    public GameObject HealthChangeEffectPrefab;
+    public float HealthChangeEffectLifeTime = 1f;
+    public float HealthChangeEffectSpeed = 1f;
+    public Color PositiveHealthChangeColor = Color.green;
+    public Color NegativeHealthChangeColor = Color.red;
 
     private float _maxHealth = 0f;
     private float _currentHealth = 0f;
+    private List<GameObject> _healthChangeEffects = new List<GameObject>();
+
+    private void Update()
+    {
+        for (int i = 0; i < _healthChangeEffects.Count; i++)
+        {
+            if (_healthChangeEffects[i] == null)
+            {
+                _healthChangeEffects.RemoveAt(i);
+            }
+            else
+            {
+                var healthChangeEffect = _healthChangeEffects[i];
+                healthChangeEffect.transform.Translate(new Vector3(0, HealthChangeEffectSpeed * Time.deltaTime, 0), Space.Self);
+
+                var textObject = healthChangeEffect.GetComponentInChildren<TextMeshProUGUI>();
+                if (textObject != null)
+                {
+                    textObject.color = new Color(
+                        textObject.color.r,
+                        textObject.color.g,
+                        textObject.color.b,
+                        textObject.color.a - HealthChangeEffectSpeed * Time.deltaTime);
+                }
+            }
+        }
+    }
 
     public void SetMaxHealth(float maxHealth)
     {
@@ -31,6 +66,28 @@ public class HealthBar : MonoBehaviour
         if (HealthBarText != null)
         {
             HealthBarText.text = $"{_currentHealth:n0}/{_maxHealth:n0}";
+        }
+    }
+
+    public void ShowChangeHealthEffect(float healthChangeAmount)
+    {
+        var healthChangeEffect = Instantiate(HealthChangeEffectPrefab, transform);
+        Destroy(healthChangeEffect, HealthChangeEffectLifeTime);
+
+        var textObject = healthChangeEffect.GetComponentInChildren<TextMeshProUGUI>();
+        if (textObject != null)
+        {
+            var sign = healthChangeAmount > 0 ? "+" : "-";
+            var color = healthChangeAmount > 0 ? PositiveHealthChangeColor : NegativeHealthChangeColor;
+
+            textObject.text = $"{sign}{Math.Abs(healthChangeAmount):n0}";
+            textObject.color = color;
+
+            _healthChangeEffects.Add(healthChangeEffect);
+        }
+        else
+        {
+            Debug.LogError("The health change effect prefab does not have a Text Mesh Pro component to display text");
         }
     }
 }
