2594d70404671574e3fd273bfbe59bf00f01950f
Alexander Ho <39490762+hoalexander44@users.noreply.github.com>
2021-01-14 22:00:54 -0500

    changes

---
+++ Vivarium/Assets/Visuals/Shaders/GenerateObstacles.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateObstacles : MonoBehaviour
+{
+    public GameObject highRiseRockPrefab;
+    public GameObject obstaclePrefab;
+    public GameObject underObstaclePrefab;
+    public GameObject pebbleFillerPreab;
+
+    public GameObject grassFiller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //var obstacleCoords = this.GetComponent<GetMapCoords>().GetObstacleCoords();
+        //var grid = this.GetComponent<GetMapCoords>().GetGrid();
+        //var gridObject = this.GetComponent<GetMapCoords>().GetGridObject();
+
+        //for (var z = 0; z < obstacleCoords.Count; z++)
+        //{
+        //    var tile = grid[obstacleCoords[z][0], obstacleCoords[z][1]];
+        //    Debug.Log(tile.Type);
+        //    Instantiate(obstaclePrefab, gridObject.GetWorldPositionCentered(obstacleCoords[z][0], obstacleCoords[z][1]), Random.rotation);
+        //}
+        var obstacleCoords = this.GetComponent<GetMapCoords>().GetObstacleCoords();
+        var grassCoords = this.GetComponent<GetMapCoords>().GetGrassTiles();
+
+        generateObstacles(obstaclePrefab, 0.4f, true, false, obstacleCoords, true);
+        generateObstacles(underObstaclePrefab , -0.6f, true, false, obstacleCoords, false);
+        generateObstacles(pebbleFillerPreab, 0, false, true, obstacleCoords, false);
+
+        generateObstacles(underObstaclePrefab, -1f, false, false, grassCoords, false, true);
+        generateObstacles(grassFiller, -0.3f, false, true, grassCoords, false, false);
+
+    }
+
+    public void generateObstacles(GameObject obstacle, float additionY, bool randomRot, bool isVariedHeight, List<List<int>> Coords, bool allowHighRise, bool adjustScale = false)
+    {
+        // var obstacleCoords = this.GetComponent<GetMapCoords>().GetObstacleCoords();
+        var obstacleCoords = Coords;
+        var grid = this.GetComponent<GetMapCoords>().GetGrid();
+        var gridObject = this.GetComponent<GetMapCoords>().GetGridObject();
+
+        for (var z = 0; z < obstacleCoords.Count; z++)
+        {
+            var tile = grid[obstacleCoords[z][0], obstacleCoords[z][1]];
+            Debug.Log(tile.Type);
+            var rotation = Quaternion.identity;
+            if (randomRot)
+            {
+                rotation = Random.rotation;
+            }
+            var obstacleInstance = Instantiate(obstacle, gridObject.GetWorldPositionCentered(obstacleCoords[z][0], obstacleCoords[z][1]) + new Vector3(0, additionY, 0), rotation);
+
+            if (allowHighRise)
+            {
+                // highrise
+                var randomoffsetX = Random.Range(-0.3f, 0.3f);
+                var randomoffsetZ = Random.Range(-0.3f, 0.3f);
+                var createhighRise = Random.Range(0, 3);
+                if (createhighRise == 1)
+                {
+                    var highRiseInstance = Instantiate(highRiseRockPrefab, gridObject.GetWorldPositionCentered(obstacleCoords[z][0], obstacleCoords[z][1]) + new Vector3(0, additionY, 0) + new Vector3(randomoffsetX, 0, randomoffsetZ), Quaternion.identity);
+                    var randRotX = Random.Range(-20, 20);
+                    var randRotY = Random.Range(0f, 180f);
+                    var randRotZ = Random.Range(-20, 20);
+                    highRiseInstance.transform.rotation *= Quaternion.Euler(randRotX, randRotY, randRotZ);
+                }
+            }
+            
+            var intervalRotation = new List<float> { 90, 180, 270};
+            
+            if (isVariedHeight)
+            {
+                var randomIntervalRotation = Random.Range(0, 2);
+                //var randHeight = Random.Range(1f, 1.5f);
+                //obstacleInstance.transform.localScale = new Vector3(1, randHeight, 1);
+                obstacleInstance.transform.rotation *= Quaternion.Euler(0, intervalRotation[randomIntervalRotation], 0);
+            }
+
+            if (adjustScale)
+            {
+                var scaleX = Random.Range(2f, 3f);
+                var scaleZ = Random.Range(2f, 3f);
+                obstacleInstance.transform.localScale = new Vector3(scaleX, 1, scaleZ);
+                //obstacleInstance.transform.localScale.x = 2;
+            }
+        }
+    }
+
+}
+
+
---
+++ Vivarium/Assets/Visuals/Shaders/GetMapCoords.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetMapCoords : MonoBehaviour
+{
+    private Grid<Tile> _grid;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //var tiles = _grid.GetGrid();
+        //var myTile = tiles[0, 0];
+        //for (int x = 0; x < tiles.GetLength(0); x += 1){
+        //    for (int z = 0; z < tiles.GetLength(1); z += 1)
+        //    {
+        //        Debug.Log(tiles[x, z].Type);
+        //    }
+        //}
+    }
+
+    public Tile[,] GetGrid()
+    {
+        return TileGridController.Instance.GetGrid().GetGrid();
+    }
+
+    public Grid<Tile> GetGridObject()
+    {
+        return TileGridController.Instance.GetGrid();
+    }
+
+    public List<List<int>> GetWaterCoords()
+    {
+        var returnList = new List<List<int>>();
+
+        _grid = TileGridController.Instance.GetGrid();
+        var tiles = _grid.GetGrid();
+        Debug.Log(tiles);
+        var myTile = tiles[0, 0];
+        for (int x = 0; x < tiles.GetLength(0); x += 1)
+        {
+            for (int z = 0; z < tiles.GetLength(1); z += 1)
+            {
+                if (tiles[x, z].Type == TileType.Water)
+                {
+                    returnList.Add(new List<int> { x, z });
+                }
+            }
+        }
+        return returnList;
+
+    }
+
+
+    public List<List<int>> GetObstacleCoords()
+    {
+        var returnList = new List<List<int>>();
+
+        _grid = TileGridController.Instance.GetGrid();
+        var tiles = _grid.GetGrid();
+        Debug.Log(tiles);
+        var myTile = tiles[0, 0];
+        for (int x = 0; x < tiles.GetLength(0); x += 1)
+        {
+            for (int z = 0; z < tiles.GetLength(1); z += 1)
+            {
+                if (tiles[x, z].Type == TileType.Obstacle)
+                {
+                    returnList.Add(new List<int> { x, z });
+                }
+            }
+        }
+        return returnList;
+
+    }
+
+
+    public List<List<int>> GetGrassTiles()
+    {
+        var returnList = new List<List<int>>();
+
+        _grid = TileGridController.Instance.GetGrid();
+        var tiles = _grid.GetGrid();
+        Debug.Log(tiles);
+        var myTile = tiles[0, 0];
+        for (int x = 0; x < tiles.GetLength(0); x += 1)
+        {
+            for (int z = 0; z < tiles.GetLength(1); z += 1)
+            {
+                if (tiles[x, z].Type == TileType.Grass)
+                {
+                    returnList.Add(new List<int> { x, z });
+                }
+            }
+        }
+        return returnList;
+
+    }
+
+
+}
