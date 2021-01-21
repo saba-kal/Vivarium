bc21646efbf70035a8237da9ef5b18d018169f8d
dlougharn <dpl49@drexel.edu>
2021-01-20 20:17:56 -0500

    Changes for Dynamic Portrait Creation
    Portraits can be created dynamically now using the portrait prefab. Portraits are retrieved by the character's portrait attribute.

--- Vivarium/Assets/Scripts/UI/PrepMenuUIController.cs
+++ Vivarium/Assets/Scripts/UI/PrepMenuUIController.cs
@@ -3,6 +3,7 @@ using UnityEngine.UI;
 using TMPro;
 using System.Collections.Generic;
 using Assets.Scripts.UI;
+using UnityEngine.SceneManagement;
 
 public class PrepMenuUIController : MonoBehaviour
 {
@@ -11,19 +12,36 @@ public class PrepMenuUIController : MonoBehaviour
 
     public Button BackButton;
     public Button InventoryButton;
-    public Dictionary<string, Sprite> CharacterPortraits = new Dictionary<string, Sprite>();
+    public GameObject PortraitPrefab;
+    public GameObject CharacterPortraits;
 
     private void Start()
     {
         BackButton.onClick.AddListener(() => OnBackClick?.Invoke());
-        GetCharacterPortraits();
+        InsertPortraits();
+
     }
 
-    public void GetCharacterPortraits()
+    public void InsertPortraits()
     {
+        int x = -740;
+        int y = 250;
         for (int i = 0; i < TurnSystemManager.Instance.PlayerController.PlayerCharacters.Count; i++)
         {
-            CharacterPortraits.Add(TurnSystemManager.Instance.PlayerController.PlayerCharacters[i].Character.Id, TurnSystemManager.Instance.PlayerController.PlayerCharacters[i].Character.Portrait);
+            GameObject Portrait = Instantiate(PortraitPrefab, new Vector3(x, y, 0), Quaternion.identity);
+            SceneManager.MoveGameObjectToScene(Portrait, SceneManager.GetSceneByName("InGameUI"));
+            Portrait.transform.SetParent(CharacterPortraits.transform, false);
+            Portrait.GetComponent<Image>().sprite = TurnSystemManager.Instance.PlayerController.PlayerCharacters[i].Character.Portrait;
+
+            if (i == 3 || i == 7)
+            {
+                x += 190;
+                y = 250;
+            }
+            else
+            {
+                y -= 175;
+            }
         }
     }
 }
