900dc7f96f3e880f360310ba0c0de1ca18722fdf
saba-kal <38592380+saba-kal@users.noreply.github.com>
2021-02-06 17:41:22 -0500

    Fixed issue where duplicate items were picked from loot table.

--- Vivarium/Assets/Scripts/Items/LootTable.cs
+++ Vivarium/Assets/Scripts/Items/LootTable.cs
@@ -16,31 +16,56 @@ public class DroppableItem
 public class LootTable : ScriptableObject
 {
     public List<DroppableItem> DroppableItems;
-public List<Item> Pick(int numberOfItems) 
+
+    public List<Item> Pick(int numberOfItems)
     {
         var droppedItems = new List<Item>();
+        var droppableItems = CopyDroppableItems();
 
         for (int i = 0; i < numberOfItems; i++)
         {
             var randValue = UnityEngine.Random.Range(0f, 1f);
-            var orderedItems = DroppableItems.OrderBy(x => x.ChanceToDrop);
+
+            var orderedItems = droppableItems.OrderBy(x => x.ChanceToDrop).ToList();
             var itemWasDropped = false;
 
-            foreach (var droppableItem in orderedItems)
+            for (int j = 0; j < orderedItems.Count; j++)
             {
+                var droppableItem = orderedItems[j];
+
                 if (randValue < droppableItem.ChanceToDrop)
                 {
-                        droppedItems.Add(droppableItem.Item);
-                        itemWasDropped = true;
-                        break;
+                    droppedItems.Add(droppableItem.Item);
+                    droppableItems.Remove(droppableItem); //Remove item so we don't pick the item again.
+                    itemWasDropped = true;
+                    break;
                 }
             }
 
             if (!itemWasDropped)
             {
-                droppedItems.Add(orderedItems.Last().Item);
+                var defaultItem = orderedItems.Last();
+                droppedItems.Add(defaultItem.Item);
+                droppableItems.Remove(defaultItem); //Remove item so we don't pick the item again.
             }
-        }        
+        }
+
         return droppedItems;
+    }
+
+    private List<DroppableItem> CopyDroppableItems()
+    {
+        var droppableItems = new List<DroppableItem>();
+        foreach (var droppableItem in DroppableItems)
+        {
+            var copy = new DroppableItem
+            {
+                Item = droppableItem.Item,
+                ChanceToDrop = droppableItem.ChanceToDrop
+            };
+            droppableItems.Add(copy);
+        }
+
+        return droppableItems;
     }
 }
