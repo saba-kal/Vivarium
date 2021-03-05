37e8e2f2281d9f920ed37c40645004943ef96939
Alexander Ho <39490762+hoalexander44@users.noreply.github.com>
2021-03-05 01:48:59 -0500

    Visual branch again (#88)
    * added_outside_objects
    
    * added materials
    
    * more_visual_updates
    
    * a lot more visual changes also camera
    
    * Revert "Merge branch 'main' into visual_branch_again"
    
    This reverts commit 319d5b2749b8b8661e9f6e001c0db227b9c5c1cc, reversing
    changes made to 5b8d8d45623c4b88529963e584a8e6d1235836d4.
    
    * backup_visuals
    
    * Revert "Revert "Merge branch 'main' into visual_branch_again""
    
    This reverts commit cdee22ed0428574156f7093a76eee406b2570414.
    
    * revert
    
    * merge main into visual branch
    
    * Revert "merge main into visual branch"
    
    This reverts commit be90c88e616df54c3923f0ae9357e3908e478177.
    
    * added bar walls to vivarium and changed outlines
    
    * removed uneeded objects in maingame
    
    * created_backup_scene
    
    * created_visual_backup

---
+++ Vivarium/Assets/Tests/EditMode/EnvGenTest.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class EnvGenTest
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void GetObstacleCoordsTest()
+        {
+            // Use the Assert class to test conditions
+        }
+
+        [Test]
+        public void CreateObstacleListTest()
+        {
+            // Use the Assert class to test conditions
+        }
+
+        [Test]
+        public void GetGrassCoordsTest()
+        {
+            // Use the Assert class to test conditions
+        }
+
+        [Test]
+        public void GetWaterCoordsTest()
+        {
+            // Use the Assert class to test conditions
+        }
+
+        [Test]
+        public void GetCoastalCoordsTest()
+        {
+            // Use the Assert class to test conditions
+        }
+
+
+    }
+}
