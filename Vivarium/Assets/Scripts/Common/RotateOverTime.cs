eb4665e0075b6e70f9f61f1c7ae4352dd2d5fca7
saba-kal <38592380+saba-kal@users.noreply.github.com>
2021-04-07 23:31:24 -0400

    Reworked main menu UI.

---
+++ Vivarium/Assets/Scripts/Common/RotateOverTime.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Slowly rotates object over time about an axis.
+/// </summary>
+public class RotateOverTime : MonoBehaviour
+{
+    public Vector3 RotationAxis = new Vector3(0, 0, 1);
+    public float RotationSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var axis = RotationAxis.normalized;
+        transform.Rotate(axis * RotationSpeed * Time.deltaTime);
+    }
+}
